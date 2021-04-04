16d605d6ef54b8599f064757526aa67fd2f72f26
justintjoa <justintoja@gmail.com>
2021-04-04 13:44:14 -0700

    added secrets, removed unnecessary providers

---
+++ lib/api_example_web/entities/Excercise.ex
@@ -0,0 +1,13 @@
+defmodule Excercise do
+    use Towel
+
+
+    
+    def init(weight, reps, excerciseName, RPE) do
+        %{weight: weight,
+        reps: reps,
+        excerciseName: excerciseName,
+        RPE: RPE}
+    end
+
+end
--- lib/api_example_web/repositories/SetPrimaryMovementRepository.ex
+++ lib/api_example_web/repositories/SetPrimaryMovementRepository.ex
@@ -10,11 +10,11 @@ defmodule ApiExample.SetPrimaryMovementRepository do
         Towel.unwrap(operation)
     end
 
-    def addTargetExcercise(repository, userName, excerciseName, weight, reps) do
-        resolveOperation(repository.provider.addTargetExcercise(userName, excerciseName, weight, reps))
+    def addTargetExcercise(repository, userName, excerciseName, weight, reps, RPE) do
+        resolveOperation(repository.provider.addTargetExcercise(userName, excerciseName, weight, reps, RPE))
     end
 
-    def removeTargetExcercise(repository, userName, excerciseName) do
+    def removeTargetExcercise(repository, userName, excerciseName, RPE) do
         resolveOperation(repository.provider.removeTargetExcercise(userName, excerciseName))
     end
 
